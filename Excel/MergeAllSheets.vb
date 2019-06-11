1d7dc677d015abc875198dc38bcc2f94467678f7
Aquafortis <support@320up.com>
2019-06-11 19:49:53 +1000

    Initial commit

---
+++ Excel/MergeAllSheets.vb
@@ -0,0 +1,18 @@
+Sub MergeAllSheets()
+    ' Merge all sheets into one
+    ' Keep top row of first sheet only
+    Dim S As Integer
+    Dim cRng As Range
+    'On Error Resume Next
+    Sheets.Add(Before:=Sheets(1)).Name = "Merged"
+    Sheets(2).Range("A1").EntireRow.Copy _
+    Sheets(1).Range("A1")
+    For S = 2 To Sheets.Count
+        Sheets(S).Activate
+        Set cRng = ActiveSheet.UsedRange
+        cRng.Offset(1, 0).Resize(cRng.Rows.Count - 1, _
+        cRng.Columns.Count).Copy _
+        Sheets(1).Range("A65536").End(xlUp)(2)
+    Next
+    Sheets("Merged").Activate
+End Sub
---
+++ README.md
@@ -0,0 +1,5 @@
+# VBA Sub Snippets
+### A collection of Subs for everyday use
+Excel Subs can be pasted into your PERSONAL.XLSB VBAProject.
+
+Chop, change, add, or edit as necessary.
